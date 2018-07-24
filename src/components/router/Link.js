f76f384ab7ce5db9db3a28c9da93ab05bef8b041
Sávio Mendes <saviomd@gmail.com>
2018-07-24 14:26:50 -0300

    Lesson 17

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -6,12 +6,17 @@ const propTypes = {
 }
 
 export class Link extends Component {
+  static contextTypes = {
+    linkHandler: PropTypes.func,
+    route: PropTypes.string,
+  }
   handleClick = (evt) => {
     evt.preventDefault()
-    window.history.pushState(null, '', this.props.to)
+    this.context.linkHandler(this.props.to)
   }
   render() {
-    return <a href="#" onClick={this.handleClick}>{this.props.children}</a>
+    const activeClass = this.context.route === this.props.to ? 'active' : ''
+    return <a className={activeClass} href="#" onClick={this.handleClick}>{this.props.children}</a>
   }
 }
 
